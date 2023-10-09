7efc7718054db4c32f81884bb922f0b6a72f9092
Göth Radim <radim.goeth@ysoft.com>
2023-10-09 15:06:09 +0200

    extract formatting / 100

--- python/statement.py
+++ python/statement.py
@@ -7,7 +7,7 @@ def statement(invoice, plays):
     result = f'Statement for {invoice["customer"]}\n'
 
     def format_as_dollars(amount):
-        return f"${amount:0,.2f}"
+        return f"${(amount / 100):0,.2f}"
 
     for perf in invoice['performances']:
         play = get_play(perf, plays)
@@ -15,13 +15,13 @@ def statement(invoice, plays):
 
         volume_credits = calculate_volume_credits(perf, play, volume_credits)
         # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(amount_per_performance / 100)} ({perf["audience"]} seats)\n'
+        result += f' {play["name"]}: {format_as_dollars(amount_per_performance)} ({perf["audience"]} seats)\n'
 
     for perf in invoice['performances']:
         amount_per_performance = calculate_amount(perf, plays)
         total_amount += amount_per_performance
 
-    result += f'Amount owed is {format_as_dollars(total_amount / 100)}\n'
+    result += f'Amount owed is {format_as_dollars(total_amount)}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
 
@@ -49,9 +49,7 @@ def calculate_amount(perf, plays):
 
 
 def calculate_volume_credits(perf, play, volume_credits):
-    # add volume credits
     volume_credits += max(perf['audience'] - 30, 0)
-    # add extra credit for every ten comedy attendees
     if "comedy" == play["type"]:
         volume_credits += math.floor(perf['audience'] / 5)
     return volume_credits
