b989e5d1fa5a4c55879a546e4e1814569bf90828
Göth Radim <radim.goeth@ysoft.com>
2023-10-09 15:02:21 +0200

    extract plays

--- python/statement.py
+++ python/statement.py
@@ -10,25 +10,28 @@ def statement(invoice, plays):
         return f"${amount:0,.2f}"
 
     for perf in invoice['performances']:
-        play = plays[perf['playID']]
+        play = get_play(perf, plays)
         amount_per_performance = calculate_amount(perf, plays)
 
         volume_credits = calculate_volume_credits(perf, play, volume_credits)
-         # print line for this order
-        result += f' {play["name"]}: {format_as_dollars(amount_per_performance/100)} ({perf["audience"]} seats)\n'
+        # print line for this order
+        result += f' {play["name"]}: {format_as_dollars(amount_per_performance / 100)} ({perf["audience"]} seats)\n'
 
     for perf in invoice['performances']:
         amount_per_performance = calculate_amount(perf, plays)
         total_amount += amount_per_performance
 
-
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
+    result += f'Amount owed is {format_as_dollars(total_amount / 100)}\n'
     result += f'You earned {volume_credits} credits\n'
     return result
 
 
+def get_play(perf, plays):
+    return plays[perf['playID']]
+
+
 def calculate_amount(perf, plays):
-    play = plays[perf['playID']]
+    play = get_play(perf, plays)
     if play['type'] == "tragedy":
         this_amount = 40000
         if perf['audience'] > 30:
@@ -56,4 +59,3 @@ def calculate_volume_credits(perf, play, volume_credits):
 
 def statement_html(invoice, plays):
     return "abc"
-
